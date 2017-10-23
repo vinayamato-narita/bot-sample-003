fe1838e97e9049f5825e36a7284da7e1ad7579e8
vinayamato-narita <narita@vinayamato.com>
2017-10-23 18:50:52 +0900

    commit from localpc

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=bot-sample-003-Web-Deploy.pubxml -p:Password=YXoK620hF9jfJ2A9g8ijmu1XcEtb6AleLNpSfz1siqusfhvt1b7go065T2qa
+
